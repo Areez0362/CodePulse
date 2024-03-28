9b09a53651b542aee2cf8e6b0b3fc1e0499fbc49
Areez0362 <ranashaka06@gmail.com>
2024-03-28 22:27:09 +0500

    Added Migrations for Db

--- API/CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
+++ API/CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a95e0cbc0fd5684e20eab8acbfeaacf2da0612c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24dc4e14272d88b129a62d5f643e836638f49886")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
